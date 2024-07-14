311b97db556571c8bc3783120269e62cd2f9277b
LdevWicks <wicks.ladonia@gmail.com>
2024-07-14 18:00:19 -0400

    Update Entertainment page

--- src/app/entrepreneurship/entrepreneurship.component.ts
+++ src/app/entrepreneurship/entrepreneurship.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import { YekizeComponent } from '../yekize/yekize.component';
 
 @Component({
   selector: 'app-entrepreneurship',
   standalone: true,
-  imports: [MatCardModule,YekizeComponent],
+  imports: [MatCardModule,YekizeComponent,CommonModule],
   templateUrl: './entrepreneurship.component.html',
   styleUrls: ['./entrepreneurship.component.css']
 })
 export class EntrepreneurshipComponent {
+  bannerItems: string[] = [
+    'Banner Item 1',
+    'Banner Item 2',
+    'Banner Item 3',
+    // Add more items as needed
+  ];
 
 }

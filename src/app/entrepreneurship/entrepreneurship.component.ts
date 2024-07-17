3622d488106bb48be49e7a2264ea6ee5602e0b59
LdevWicks <wicks.ladonia@gmail.com>
2024-07-16 21:18:34 -0400

    Updated Entrpreneur section

--- src/app/entrepreneurship/entrepreneurship.component.ts
+++ src/app/entrepreneurship/entrepreneurship.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import { YekizeComponent } from '../yekize/yekize.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { RouterModule, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-entrepreneurship',
   standalone: true,
-  imports: [ MatCardModule,YekizeComponent,CommonModule,MatTabsModule],
+  imports: [ MatCardModule,YekizeComponent,CommonModule,MatTabsModule, RouterModule],
   templateUrl: './entrepreneurship.component.html',
   styleUrls: ['./entrepreneurship.component.css']
 })
@@ -18,5 +21,38 @@ export class EntrepreneurshipComponent {
     'Banner Item 3',
     // Add more items as needed
   ];
+  
+  ceo$: Observable<any[]> = new Observable<any[]>();
 
+  constructor(private router: Router, private firestore: AngularFirestore,) {}
+  ngOnInit(): void {
+    this.ceo$ = this.firestore.collection('ceo').valueChanges({ idField: 'id' });
+  }
+  
+  navigateToYekize(): void {
+    this.router.navigate(['/yekize']);
+  }
+
+  navigateToLAWND(): void {
+    this.router.navigate(['/lawnd']);
+  }
+ 
+  navigateToCW(): void {
+    this.router.navigate(['/cw']);
+  }
+
+  navigateToPage(productId: string): void {
+    switch (productId) {
+      case 'lawnd':
+        this.navigateToLAWND();
+        break;
+      
+      case 'yekize':
+        this.navigateToYekize();
+        break;
+      default:
+        console.log('Unknown product ID:', productId);
+    }
+
+}
 }
---
+++ src/app/lawnd/lawnd.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-lawnd',
+  standalone: true,
+  imports: [ MatCardModule],
+  templateUrl: './lawnd.component.html',
+  styleUrl: './lawnd.component.css'
+})
+export class LAWNDComponent {
+
+}

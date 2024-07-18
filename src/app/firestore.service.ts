6ed3a5dce9c44f15f7b257354295164f6426b5fa
LdevWicks <wicks.ladonia@gmail.com>
2024-07-17 22:58:34 -0400

    Add disscusio board style

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,28 +1,27 @@
 // firestore.service.ts
 
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
-  private postsCollection: AngularFirestoreCollection<any>;
 
-  constructor(private afs: AngularFirestore) {
-    this.postsCollection = this.afs.collection('posts');
-  }
-
-  
+  constructor(private firestore: AngularFirestore) { }
 
   getPosts(): Observable<any[]> {
-    return this.afs.collection('posts').valueChanges();
+    return this.firestore.collection('posts').valueChanges({ idField: 'id' });
   }
 
-  addPost(post: any): Promise<any> {
-    return this.postsCollection.add(post);
+  addPost(post: any): Promise<void> {
+    const id = this.firestore.createId();
+    return this.firestore.collection('posts').doc(id).set({ ...post, id });
+  }
+
+  updatePost(id: string, data: any): Promise<void> {
+    return this.firestore.collection('posts').doc(id).update(data);
   }
 }
 

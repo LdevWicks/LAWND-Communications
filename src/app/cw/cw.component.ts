dec85bb413693cf98e1ae102eefee3190aa45ead
LdevWicks <wicks.ladonia@gmail.com>
2024-07-18 16:39:30 -0400

    Updated difference

--- src/app/cw/cw.component.ts
+++ src/app/cw/cw.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-cw',
   standalone: true,
-  imports: [MatListModule],
+  imports: [ CommonModule, MatCardModule, MatListModule, MatTooltipModule, MatIconModule],
   templateUrl: './cw.component.html',
   styleUrls: ['./cw.component.css']
 })
 export class CwComponent {
 
+  images: string[] = [
+    'assets/CWLogo.png',
+  ];
 }

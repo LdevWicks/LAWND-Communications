31c186242e7431784dab0c2119112c02a08195f9
LdevWicks <wicks.ladonia@gmail.com>
2024-07-17 22:58:34 -0400

    Add disscusio board style

--- src/app/discussion-board/discussion-board.component.ts
+++ src/app/discussion-board/discussion-board.component.ts
@@ -7,13 +7,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-discussion-board',
   standalone: true,
   templateUrl: './discussion-board.component.html',
   styleUrls: ['./discussion-board.component.css'],
-  imports:[ MatCardModule, MatDividerModule, MatFormFieldModule, CommonModule,FormsModule],
+  imports:[MatCardModule, MatIconModule, MatDividerModule, MatFormFieldModule, CommonModule,FormsModule],
 })
 export class DiscussionBoardComponent implements OnInit {
   posts: any[] = [];
@@ -26,14 +27,43 @@ export class DiscussionBoardComponent implements OnInit {
 
   loadPosts(): void {
     this.firestoreService.getPosts().subscribe(posts => {
-      this.posts = posts;
+      this.posts = posts.map(post => {
+        return {
+          ...post,
+          liked: false,
+          showReplyForm: false,
+          replies: post.replies || []
+        };
+      });
     });
   }
 
   addPost(postForm: NgForm): void {
-    const postData = postForm.value;
+    const postData = {
+      ...postForm.value,
+      likes: 0,
+      replies: []
+    };
     this.firestoreService.addPost(postData).then(() => {
-      // Handle successful post addition (e.g., reset form, show notification)
-    })
+      postForm.reset();
+      this.loadPosts();
+    });
+  }
+
+  toggleLike(post: any): void {
+    post.liked = !post.liked;
+    post.likes = post.likes || 0; // Ensure likes is initialized
+    post.likes += post.liked ? 1 : -1;
+    this.firestoreService.updatePost(post.id, { likes: post.likes });
+  }
+
+  addReply(post: any, replyForm: NgForm): void {
+    if (replyForm.valid) {
+      post.replies.push(replyForm.value.replyContent);
+      this.firestoreService.updatePost(post.id, { replies: post.replies }).then(() => {
+        post.showReplyForm = false;
+        replyForm.reset();
+      });
+    }
   }
 }

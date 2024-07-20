088acb8473c98718375c33f26da86b50c28fae29
LdevWicks <wicks.ladonia@gmail.com>
2024-07-19 22:43:40 -0400

    Update dmovie section

--- src/app/movie-dialog/movie-dialog.component.ts
+++ src/app/movie-dialog/movie-dialog.component.ts
@@ -2,18 +2,19 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-dialog',
   standalone: true,
-  imports:[MatDialogModule, MatButtonModule, MatCardModule],
+  imports:[MatDialogModule, CommonModule, MatButtonModule, MatCardModule],
   templateUrl: './movie-dialog.component.html',
   styleUrls: ['./movie-dialog.component.css']
 })
 export class MovieDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<MovieDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { title: string; description: string; imdbLink: string; image:string; } // Injecting data using MAT_DIALOG_DATA
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; description: string; imdbLink: string; image:string;  videoUrl:string;} // Injecting data using MAT_DIALOG_DATA
   ) {}
 
   onClose(): void {

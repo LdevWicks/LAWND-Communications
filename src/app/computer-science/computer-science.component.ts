cb0fd34d6b478fc6db4bf77a82f9411e43317f45
LdevWicks <wicks.ladonia@gmail.com>
2024-07-19 20:00:02 -0400

    Updated Computer Science

--- src/app/computer-science/computer-science.component.ts
+++ src/app/computer-science/computer-science.component.ts
@@ -4,16 +4,30 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatDialog } from '@angular/material/dialog';
 import {AggiePrideDialogComponent} from '../aggie-pride-dialog/aggie-pride-dialog.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MatStepperModule} from '@angular/material/stepper'
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-computer-science',
   standalone: true,
-  imports: [MatCardModule, MatToolbarModule, MatTabsModule],
+  imports: [ MatCardModule,CommonModule, ReactiveFormsModule, MatToolbarModule,MatStepperModule, MatTabsModule],
   templateUrl: './computer-science.component.html',
   styleUrls: ['./computer-science.component.css']
 })
 export class ComputerScienceComponent implements OnInit{
-  constructor(private dialog: MatDialog) {}
+
+  stepControl: FormGroup;
+  facts = [
+    { year: '2006', fact: ' 1st project Hello World 2006', image: 'assets/helloWorld.png' },
+    { year: '2007', fact: 'freshman year build lego pick up object program i t to move not using hands' },
+    { year: '2008', fact: 'Fact for 2008' },
+    { year: '2009', fact: 'Fact for 2009' },
+    { year: '2010', fact: 'made tshirt website add logo, teacher hacked site password admin admin' }
+  ];
+
+  constructor(private dialog: MatDialog, private formBuilder: FormBuilder) { 
+    this.stepControl = this.formBuilder.group({});}
 
   ngOnInit(): void {
     this.openAggiePrideDialog();
@@ -26,6 +40,17 @@ export class ComputerScienceComponent implements OnInit{
       console.log('Dialog closed with result:', result);
     });
   }
+
+
+  isFirstStep(fact: { year: string, fact: string }): boolean {
+    return this.facts.indexOf(fact) === 0;
+  }
+
+  isLastStep(fact: { year: string, fact: string }): boolean {
+    return this.facts.indexOf(fact) === this.facts.length - 1;
+  }
+
+
 }
 
 

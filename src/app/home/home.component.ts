d9a2520c8ddaf144fbb58ce0be43dc1fe40b6e7c
LdevWicks <wicks.ladonia@gmail.com>
2024-07-22 14:18:07 -0400

    Updated homepage layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,28 @@
 import { Component , OnInit} from '@angular/core';
 import { MatCardModule} from'@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule} from '@angular/material/grid-list';
 import { ComputerScienceComponent } from '../computer-science/computer-science.component';
 import { CybersecurityComponent } from '../cybersecurity/cybersecurity.component';
 import { EntertainmentComponent } from '../entertainment/entertainment.component';
 import { EntrepreneurshipComponent } from '../entrepreneurship/entrepreneurship.component';
 import { NonprofitComponent } from '../nonprofit/nonprofit.component';
-import {FormBuilder, FormGroup, Validators, FormsModule} from '@angular/forms';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormGroup, Validators, FormsModule} from '@angular/forms';
+import { MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule} from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatDividerModule} from '@angular/material/divider';
-import { Router } from '@angular/router';
+import { MatDividerModule} from '@angular/material/divider';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css',],
-    imports: [MatCardModule,MatDividerModule,CommonModule,ReactiveFormsModule, MatInputModule, MatSelectModule,MatFormFieldModule,FormsModule, MatStepperModule, MatGridListModule,NonprofitComponent, ComputerScienceComponent,CybersecurityComponent, EntertainmentComponent,EntrepreneurshipComponent]
+    imports: [ MatCardModule, RouterModule, MatIconModule, MatDividerModule,CommonModule,ReactiveFormsModule, MatInputModule, MatSelectModule,MatFormFieldModule,FormsModule, MatStepperModule, MatGridListModule,NonprofitComponent, ComputerScienceComponent,CybersecurityComponent, EntertainmentComponent,EntrepreneurshipComponent]
 })
 export class HomeComponent implements OnInit{
   categories: string[] = [];
@@ -29,6 +30,26 @@ export class HomeComponent implements OnInit{
   years: number[] = Array.from({ length: 19 }, (_, index) => 2006 + index); // Array of years from 2006 to 2024
   yearFormGroup!: FormGroup;
   message: string;
+
+  testimonials = [
+    { name: 'John Doe', position: 'CEO', message: 'This app is amazing!' },
+    { name: 'Jane Smith', position: 'CTO', message: 'Highly recommend this to everyone.' },
+    { name: 'Bob Johnson', position: 'Developer', message: 'Best app I have ever used.' },
+  ];
+
+  updates = [
+    { title: 'Update 1', date: new Date(), description: 'Description of update 1' },
+    { title: 'Update 2', date: new Date(), description: 'Description of update 2' },
+    { title: 'Update 3', date: new Date(), description: 'Description of update 3' },
+  ];
+
+  features = [
+    { icon: 'build', title: 'Nonprofit', description: 'Learn more about LAWND || Communication nonprofit work', image: 'assets/npceo.png', route:'/nonprofit' },
+    { icon: 'security', title: 'Computer Science', description: 'NC A&T 2006-2010', image: 'assets/AggiePride.jpg', route:'/computer-science' },
+    { icon: 'update', title: 'Entertainment', description: 'wnt ....', image: 'assets/entceo.png',route:'/entertainment' },
+    { icon: 'update', title: 'Entrepreneurship', description: 'wnt ....', image: 'assets/mylogos.png',route:'/entreprenuership' },
+    { icon: 'update', title: 'Cyber Security', description: 'wnt ....', image: 'assets/certs.png',route:'/cyber-security' },
+  ];
 
   constructor(private formBuilder: FormBuilder, private router: Router) { 
      this.categories = ["Computer Science", "Entertainment", "Nonprofit", "Entrepreneurship", "Cybersecurity"];

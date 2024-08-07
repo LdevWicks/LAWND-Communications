fe203c15cd8547c8c25f5ff4ecab8b109259059c
LdevWicks <wicks.ladonia@gmail.com>
2024-08-07 00:24:16 -0400

    Added dashboard metrics

--- src/app/advance-features/dashboard/dashboard.component.ts
+++ src/app/advance-features/dashboard/dashboard.component.ts
@@ -25,20 +25,31 @@ export class DashboardComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   getVulnerabilityMetrics() {
-    this.http.get('/api/vulnerabilities/metrics').subscribe((data) => {
+    this.http.get('http://localhost:3000/api/vulnerabilities/metrics').subscribe((data) => {
       this.vulnerabilityMetrics = data;
     });
   }
 
   getComplianceMetrics() {
-    this.http.get('/api/compliance/metrics').subscribe((data) => {
+    this.http.get('http://localhost:3000/api/compliance/metrics').subscribe((data) => {
       this.complianceMetrics = data;
     });
   }
 
   getIncidentMetrics() {
-    this.http.get('/api/incidents/metrics').subscribe((data) => {
-      this.incidentMetrics = data;
-    });
-  }
+    this.http.get('http://localhost:3000/api/incidents/metrics').subscribe(
+        (data) => {
+            this.incidentMetrics = data;
+        },
+        (error) => {
+            console.error('Error fetching incident metrics:', error);
+        }
+    );
 }
+
+  
+  }
+  
+
+  
+

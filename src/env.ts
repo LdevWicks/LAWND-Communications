e3ddab40a488acfc6c93e284b126bacfe04d2440
LdevWicks <wicks.ladonia@gmail.com>
2024-08-11 13:00:41 -0400

    Removing gitignorfiles

---
+++ src/env.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: 'undefined',
+        authDomain: 'undefined',
+        databaseURL: 'undefined',
+        projectId: 'undefined',
+        storageBucket: 'undefined',
+        messagingSenderId: 'undefined',
+        appId: 'undefined',
+        measurementId: 'undefined'
+    }
+};

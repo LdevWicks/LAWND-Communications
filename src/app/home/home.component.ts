d55dccdee9136d1e7d604970f64b018e2a10eb40
LdevWicks <wicks.ladonia@gmail.com>
2024-08-12 00:07:41 -0400

    Updated Innovative content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,37 @@
-import { Component , OnInit} from '@angular/core';
-import { MatCardModule} from'@angular/material/card';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { ComputerScienceComponent } from '../computer-science/computer-science.component';
 import { CybersecurityComponent } from '../cybersecurity/cybersecurity.component';
 import { EntertainmentComponent } from '../entertainment/entertainment.component';
 import { EntrepreneurshipComponent } from '../entrepreneurship/entrepreneurship.component';
 import { NonprofitComponent } from '../nonprofit/nonprofit.component';
-import { MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatStepperModule } from '@angular/material/stepper';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css',],
-    imports: [ MatCardModule,MatStepperModule, RouterModule, MatIconModule, CommonModule,NonprofitComponent, ComputerScienceComponent,CybersecurityComponent, EntertainmentComponent,EntrepreneurshipComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  imports: [
+    MatCardModule,
+    MatStepperModule,
+    RouterModule,
+    MatIconModule,
+    CommonModule,
+    NonprofitComponent,
+    ComputerScienceComponent,
+    CybersecurityComponent,
+    EntertainmentComponent,
+    EntrepreneurshipComponent,
+  ],
 })
-export class HomeComponent implements OnInit{
-  categories: string[] = [];
- 
+export class HomeComponent implements AfterViewInit , OnInit{
 
+  categories: string[] = [];
+  @ViewChild('videoPlayer') videoPlayer: ElementRef<HTMLVideoElement>;
 
   updates = [
     { title: 'Update 001.21', date: new Date(), description: 'Multifactor Authentication enabled.' },
@@ -37,21 +48,32 @@ export class HomeComponent implements OnInit{
     { icon: 'security', title: 'Advanced Features', description: 'Unlock & Explore', image: 'assets/1.png',route:'/mfa-login' },
   ];
 
-
   constructor(private router: Router) { 
-     this.categories = ["Computer Science", "Entertainment", "Nonprofit", "Entrepreneurship", "Cybersecurity"];
-  
-  }
-  
+    this.categories = ["Computer Science", "Entertainment", "Nonprofit", "Entrepreneurship", "Cybersecurity"];
+ 
+ }
+ 
 
-  navigateTo(route: string) {
-    this.router.navigate([route]);
-  }
-  ngOnInit() {
-    
-    
-  }
+ navigateTo(route: string) {
+   this.router.navigate([route]);
+ }
+ ngOnInit(): void {
+  // Initialization logic here if needed
 }
+  ngAfterViewInit(): void {
+    this.playVideo();
+  }
 
-
-
+  playVideo(): void {
+    const videoElement = this.videoPlayer?.nativeElement;
+  
+    if (videoElement) {
+      videoElement.load(); // Ensure the video is loaded
+      videoElement.play().catch(error => {
+        console.error('Error attempting to play video:', error);
+        // Additional handling if needed
+      });
+    }
+  }
+  
+}
--- src/app/nonprofit/innovative/innovative.component.ts
+++ src/app/nonprofit/innovative/innovative.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-innovative',
   standalone: true,
   templateUrl: './innovative.component.html',
-  styleUrls: ['./innovative.component.css']
+  styleUrls: ['./innovative.component.css'],
+  imports:[MatIconModule, CommonModule]
 })
 export class InnovativeComponent {
+
+  services = [
+    { name: 'Residential support', icon: 'home' },
+    { name: 'Individual habilitation Services', icon: 'person' },
+    { name: 'Skilled Nursing', icon: 'local_hospital' },
+    { name: 'Supported Employment', icon: 'work' },
+    { name: 'Prevocation services', icon: 'help' }
+  ];
 
   constructor() { }
 

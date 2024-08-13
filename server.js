4f0d8dd089437629dc02221ed18ff13c70d40aa4
LdevWicks <wicks.ladonia@gmail.com>
2024-08-13 14:38:27 -0400

    Updated env

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the Angular app
-app.use(express.static(path.join(__dirname, 'dist/path-to-ciso')));
+// Import routers
+const complianceRouter = require('./routes/compliance');
+const incidentsRouter = require('./routes/incidents');
+const vulnerabilitiesRouter = require('./routes/vulnerabilities');
+const incidentMetricsRouter = require('./routes/incidentMetrics');
+const complianceMetricsRouter = require('./routes/complianceMetrics');
+const vulnerabilitiesMetricsRouter = require('./routes/vulnerabilitiesMetrics');
 
-// Serve Angular index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/path-to-ciso/index.html'));
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(express.json()); // For parsing application/json
+app.use(express.static('dist/path-to-ciso')); // Serve Angular app
+
+// API routes
+
+app.use('/api/incidents', incidentsRouter);
+app.use('/api/vulnerabilities', vulnerabilitiesRouter);
+app.use('/api/incidents/metrics', incidentMetricsRouter);
+app.use('/api/compliance/metrics', complianceMetricsRouter);
+app.use('/api/vulnerabilities/metrics', vulnerabilitiesMetricsRouter);
+app.use('/api/compliance', complianceRouter);
+
+// Serve Angular app
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/path-to-ciso/index.html');
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-

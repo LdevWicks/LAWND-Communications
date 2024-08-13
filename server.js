fc6422335baa4b88d8ef82a3df42d029928d7eb4
LdevWicks <wicks.ladonia@gmail.com>
2024-08-13 13:08:00 -0400

    Updated env

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// server.js
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/path-to-ciso')));
+app.use(express.static('dist/path-to-ciso')); // Serve your Angular app
 
-// For all GET requests, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/path-to-ciso/index.html'));
+// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/path-to-ciso/index.html');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
